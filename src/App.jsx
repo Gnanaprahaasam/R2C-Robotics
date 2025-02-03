09b86a80faa5d5b2cfc33841c497a3c98b14a004
Gnanaprahaasam <gnanaprahaasamsr@gmail.com>
2025-02-03 15:11:59 +0530

    changes updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { HashRouter } from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer'
+
 import Header from './components/Header'
 import Robotics from './pages/Robotics/Robotics'
 
@@ -14,7 +14,7 @@ function App() {
       <HashRouter>
         <Header />
         <Robotics />
-        <Footer />
+        
       </HashRouter>
     </>
   )
